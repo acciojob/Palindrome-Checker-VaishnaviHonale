d30b32eacd7919300b7902f2fceae9d7487e07d1
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-25 12:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	 var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
 	
-	// for(let i=0;i<len/2;i++){
-	// 	if(str[i]!==str[len-1-i]){
-	// 		return false;
-	// 	}
-	// }
-	// return true;
-
-	const arrayValues=string.split('');
-	const reverseArrayValues=ArrayValues.reverse();
-	const reverseString=reverseArrayValues.join('');
-	if(str==reverseStringString){
-		return true;
-	}
-	else{
-		return false;
-	}
 }
 module.exports = palindrome
