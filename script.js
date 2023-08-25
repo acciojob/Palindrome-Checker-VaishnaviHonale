1ef18c68a10c9f0da9c541cfcc9e04e6918fae5f
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-25 12:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 
 function palindrome(str){
 	const len=str.length;
-	for(let i=0;i<len/2;i++){
-		if(str[i]!==str[len-1-i]){
-			return false;
-		}
-	}
-	return true;
+	// for(let i=0;i<len/2;i++){
+	// 	if(str[i]!==str[len-1-i]){
+	// 		return false;
+	// 	}
+	// }
+	// return true;
 
+	const arrayValues=string.split('');
+	const reverseArrayValues=reverseArrayValues.reverse();
+	const reverstringString=reverstringString.join('');
+	if(str==reverstringString){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
